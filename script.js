4385b3b75cb943e338bc7892433a30257c762457
Zena <zenanusair2002@gmail.com>
2024-11-06 15:10:03 +0300

    Initial commit with HTML, CSS, and JavaScript files

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript code to add interactivity
+document.getElementById('changeColorButton').addEventListener('click', function () {
+    document.body.style.backgroundColor =
+        document.body.style.backgroundColor === 'lightblue' ? 'white' : 'lightblue';
+});
